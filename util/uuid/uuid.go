b6ee62a12fe51299963c95594b2dac25c8939485
OnlyOneFace <13522570308@139.com>
2020-12-20 01:30:31 +0800

    uuid 增加machineid

--- util/uuid/uuid.go
+++ util/uuid/uuid.go
@@ -7,24 +7,25 @@ package uuid
 import (
 	"encoding/base32"
 	"encoding/binary"
-	"os"
 	"sync/atomic"
 	"time"
 )
 
-type UUID [19]byte
+type UUID [22]byte
 
 var (
-	version  byte = 0x01 // set version to 1
-	pid           = os.Getpid()
-	clockSeq      = randUint64()
+	version   byte = 0x01 // set version to 1
+	pid            = Pid()
+	clockSeq       = randUint64()
+	machineID      = readMachineID()
 )
 
 func New() UUID {
 	return FromTime(time.Now())
 }
 
-// 8 bytes of time (ns) + 1 bytes of version + 2 byes of pid + 8 random bytes
+// 8 bytes of time (ns) + 1 bytes of version +
+// first 3 bytes of md5(Machine hostname) + 2 byes of pid + 8 random bytes
 func FromTime(aTime time.Time) UUID {
 	var id UUID
 
@@ -33,19 +34,23 @@ func FromTime(aTime time.Time) UUID {
 	binary.BigEndian.PutUint64(id[:], uint64(utcTime.UnixNano()))
 	// version, 1 bytes
 	id[8] = version
+	// Machine, first 3 bytes of md5(hostname)
+	id[9] = machineID[0]
+	id[10] = machineID[1]
+	id[11] = machineID[2]
 	// Pid, 2 bytes, specs don't specify endianness, but we use big endian.
-	id[9] = byte(pid >> 8)
-	id[10] = byte(pid)
+	id[12] = byte(pid >> 8)
+	id[13] = byte(pid)
 	// random, 8 bytes big endian
 	clock := atomic.AddUint64(&clockSeq, 1)
-	id[11] = byte(clock >> 56)
-	id[12] = byte(clock >> 48)
-	id[13] = byte(clock >> 40)
-	id[14] = byte(clock >> 32)
-	id[15] = byte(clock >> 24)
-	id[16] = byte(clock >> 16)
-	id[17] = byte(clock >> 8)
-	id[18] = byte(clock)
+	id[14] = byte(clock >> 56)
+	id[15] = byte(clock >> 48)
+	id[16] = byte(clock >> 40)
+	id[17] = byte(clock >> 32)
+	id[18] = byte(clock >> 24)
+	id[19] = byte(clock >> 16)
+	id[20] = byte(clock >> 8)
+	id[21] = byte(clock)
 	return id
 }
 
