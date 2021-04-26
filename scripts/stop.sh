a1d94ad9c9d2a42c41ebae7e5993e5d5c1e7fe39
l30002214 <licongfu@huawei.com>
2021-04-26 11:33:17 +0800

    修改停止脚本

--- scripts/stop.sh
+++ scripts/stop.sh
@@ -2,11 +2,4 @@
 
 set -e
 
-process_id=$(ps -ef | grep "obs" | grep -v grep | awk '{print $2}')
-
-if [ -z "${process_id}" ]; then
-  echo "process id is null."
-else
-  echo pid is ${process_id}
-  kill -2 ${process_id}
-fi
+ps -ef|grep "obs"|grep -v grep|awk '{print $2}'| | xargs kill -2
