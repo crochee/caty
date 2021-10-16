151d1fba74b9f55ab337a223e3b03b5418a80474
crochee <licf137@chinaunicom.cn>
2021-10-16 19:45:29 +0800

    [Bugfix] 修改配置

--- build/package/cca/entrypoint.sh
+++ build/package/cca/entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -ex
 # docker build --no-cache -t cca .
-# docker run -itd -p 8120:8120 --restart=always --name cca-server cca
+# docker run -itd -p 8120:8120 --restart=always --name ccasrv cca
 if [ "${1:0:1}" = '-' ]; then
   set -- cca "$@"
 fi
