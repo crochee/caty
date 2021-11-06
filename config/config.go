49d172fef12dbbbba7bdeb69b15c769f5e7520a9
crochee <licf137@chinaunicom.cn>
2021-11-07 01:54:07 +0800

    [Refactor] 修改启动文件路劲

--- config/config.go
+++ config/config.go
@@ -9,11 +9,15 @@ import (
 
 // LoadConfig init Config
 func LoadConfig(path string) error {
-	viper.AddConfigPath(".")                    // 设置配置文件路径
-	viper.SetConfigName(path)                   // 设置配置文件名称
+	viper.AddConfigPath("/") // 设置配置文件路径
+	absPath, err := filepath.Abs(path)
+	if err != nil {
+		return err
+	}
+	viper.SetConfigName(absPath)                // 设置配置文件名称
 	viper.SetConfigType(filepath.Ext(path)[1:]) // 设置配置文件类型
 
-	if err := viper.ReadInConfig(); err != nil {
+	if err = viper.ReadInConfig(); err != nil {
 		return fmt.Errorf("read config failed.%w", err)
 	}
 	return nil
