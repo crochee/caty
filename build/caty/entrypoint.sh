2b218bfff02040d7f06fbc0b36e0434089da18e8
crochee <licf137@chinaunicom.cn>
2021-11-06 22:48:07 +0800

    [Refactor] 最小权限启动

--- build/caty/entrypoint.sh
+++ build/caty/entrypoint.sh
@@ -8,4 +8,12 @@ if [ "${1:0:1}" = '-' ]; then
   set -- caty "$@"
 fi
 
+# If container is started as root user, restart as dedicated dev user
+# allow the container to be started with `--user`
+if [ "$(id -u)" = "0" ]; then
+	echo "switch to user 'dev'"
+	find . \! -user dev -exec chown dev '{}' +
+	exec gosu dev "$0" "$@"
+fi
+
 exec "$@"
