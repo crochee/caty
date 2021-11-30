9da040a66f3927824a2e3e69da984104aaee2975
crochee <licf137@chinaunicom.cn>
2021-11-30 17:21:13 +0800

    [Feature] add redis cluster

--- build/package/redis/redis.sh
+++ build/package/redis/redis.sh
@@ -7,3 +7,7 @@ docker run -p 6379:6379 --name redis -d redis:latest --requirepass "123456"
 docker run --name my_redis -p 6379:6379 -v /root/docker/redis/data:/data -v /root/docker/redis/conf/redis.conf:/etc/redis/redis.conf -d redis redis-server /etc/redis/redis.conf
 
 #auth 你的密码
+
+
+
+docker run -itd -e "IP=0.0.0.0" -e STANDALONE=true -e SENTINEL=true --name redis -p 7000-7005:7000-7005 grokzen/redis-cluster:latest
