8c5960fa0322c3a0dcf1f24f8e3f72fa4f723abb
crochee <13522570308@139.com>
2021-12-15 21:45:49 +0800

    [featrue] add copy

--- internal/retry_test.go
+++ internal/retry_test.go
@@ -5,32 +5,31 @@ import (
 	"fmt"
 	"reflect"
 	"testing"
+
+	"github.com/crochee/lirity/routine"
 )
 
 type Manager struct {
-	model map[string]reflect.Type
+	model map[string]Executor
 }
 
 func (m *Manager) register(v Executor) error {
-	vt := reflect.TypeOf(v)
 	var (
-		name      string
-		typeValue reflect.Type
+		vt   = reflect.TypeOf(v)
+		name string
 	)
 	switch vt.Kind() {
 	case reflect.Struct:
 		name = vt.String()
-		typeValue = vt
 	case reflect.Ptr:
 		name = vt.Elem().String()
-		typeValue = vt.Elem()
 	default:
 		return fmt.Errorf("not support %s", vt.String())
 	}
 	if _, ok := m.model[name]; ok {
 		return fmt.Errorf("%s is enable", name)
 	}
-	m.model[name] = typeValue
+	m.model[name] = v
 	return nil
 }
 
@@ -48,42 +47,73 @@ func (m *Manager) Run(name string) error {
 	if !ok {
 		return fmt.Errorf("must register model %s", name)
 	}
-	var instance Executor
-	if instance, ok = reflect.New(v).Interface().(Executor); !ok {
-		return fmt.Errorf("%s must implement Executor interface", name)
-	}
-	return instance.Run(context.Background(), nil)
+	return v.Copy().Run(context.Background(), nil)
 }
 
 type Executor interface {
+	Copy() Executor
 	Run(ctx context.Context, data []byte) error
 }
 
 type test struct {
-	i interface{}
+}
+
+func (t test) Copy() Executor {
+	return t
 }
 
 func (t test) Run(ctx context.Context, data []byte) error {
-	fmt.Print(90)
+	fmt.Print("90\t")
 	return nil
 }
 
 type test1 struct {
-	i interface{}
+	i uint
+}
+
+func (t *test1) Copy() Executor {
+	tmp := *t
+	return &tmp
 }
 
 func (t *test1) Run(ctx context.Context, data []byte) error {
-	t.i = 91
-	fmt.Print(91)
-	fmt.Printf("%#v", t)
+	t.i++
+	fmt.Print("91\t")
+	fmt.Printf("%v \t", t)
 	return nil
 }
 
+type multiTest struct {
+	list []Executor
+}
+
+func (m *multiTest) Copy() Executor {
+	tmp := &multiTest{list: make([]Executor, 0, len(m.list))}
+	for _, e := range m.list {
+		tmp.list = append(tmp.list, e.Copy())
+	}
+	return tmp
+}
+
+func (m *multiTest) Run(ctx context.Context, data []byte) error {
+	fmt.Println(len(m.list))
+	g := routine.NewGroup(ctx)
+	for _, e := range m.list {
+		tmp := e
+		g.Go(func(ctx context.Context) error {
+			return tmp.Run(ctx, data)
+		})
+	}
+	return g.Wait()
+}
+
 func TestRetry(t *testing.T) {
-	m := &Manager{model: map[string]reflect.Type{}}
-	if err := m.Register(test{}, &test1{}); err != nil {
+	m := &Manager{model: map[string]Executor{}}
+	if err := m.Register(test{}, &test1{}, &multiTest{list: []Executor{test{}, &test1{}}}); err != nil {
 		t.Fatal(err)
 	}
 	t.Log(m.Run("internal.test"))
 	t.Log(m.Run("internal.test1"))
+	t.Log(m.Run("internal.multiTest"))
+	t.Log(m.Run("internal.multiTest"))
 }
